87ddc44ccbf40eb20486be5f8c1eeb5a73d9f80b
AlbericSimperl1 <alberic.simperl@student.kuleuven.be>
2026-07-23 01:55:25 +0200

    floating

--- PC/src/main.rs
+++ PC/src/main.rs
@@ -10,6 +10,7 @@ mod types;
 fn main() -> Result<(), eframe::Error> {
     let options = eframe::NativeOptions {
         viewport: egui::ViewportBuilder::default()
+            .with_app_id("hyprland-display-streamer")
             .with_inner_size([840.0, 400.0])
             .with_min_inner_size([720.0, 400.0]),
         ..Default::default()
